448bcbb22dbfa4dfdaab8db561d19ffb1f57e895
Ehmad <justEhmadSaeed@gmail.com>
2020-02-05 21:56:02 +0500

    ⚡IMPROVE and Commented

--- Insertion-Sort.cpp
+++ Insertion-Sort.cpp
@@ -1,64 +1,79 @@
+
+// Libraries to be added
+
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
+#include <chrono>
 #include <time.h>
 #include <fstream>
 using namespace std;
+
+// Function Prototypes
+
 void InsertionSort(int array[], int start, int end);
 void RandomArray(int array[], int size);
-int main() {
+
+int main()
+{
 	int size;
 	cout << "Enter the Size of Array: ";
 	cin >> size;
-	int* A = new int[size];
+	int *A = new int[size];
+
+	// To generate random Numbers and store them
+
 	srand(time(NULL));
 	RandomArray(A, size);
 
-	/*cout << "\n Numbers before Sorting\n to print them, ";
-	system("pause");
-	for (int i = 0; i < size; i++)
-		cout << A[i] << endl;
-*/
-	clock_t start, end;
-	start = clock();
+	// Code to call InsertionSort and measure time duration
+
+	auto start = chrono::high_resolution_clock::now();
 	InsertionSort(A, 0, size - 1);
-	end = clock();
-	double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
-	cout << "\n Time taken by the insertion Sort,\nwith " << size << " size of Array is: ";
-	cout << fixed << time_taken << setprecision(7)<<" seconds"<<endl;
+	auto end = chrono::high_resolution_clock::now();
+	double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+	// Code to calculate the time duration
+
+	time_taken *= 1e-9;
+	cout << "\n Time taken by the insertion Sort, with " << size << " size of Array is: ";
+	cout << fixed << time_taken << setprecision(9) << " seconds" << endl;
+
+	// Code to Save sorted array in csv file
 
 	ofstream OutData;
 	OutData.open("SortedInsertionSort.csv");
 	for (int i = 0; i < size; i++)
 		OutData << A[i] << endl;
-	/*cout << "\n\n Numbers after Sorting\n To print them, ";
-	system("pause");
-	for (int i = 0; i < size; i++)
-		cout << A[i] << endl;*/
+
 	delete[] A;
 }
 
 // $$$ Functions Definitions $$$
 
 // InsertionSort Iterative Function
-void InsertionSort(int arr[], int startIndex, int endIndex) {
+void InsertionSort(int arr[], int startIndex, int endIndex)
+{
 
 	int i, key, j;
-	for (i = 1; i <= endIndex; i++)
+	for (i = startIndex + 1; i <= endIndex; i++)
 	{
 		key = arr[i];
 		j = i - 1;
-		while (j >= 0 && arr[j] > key) {
+		// To place the element in the sorted array
+		while (j >= startIndex && arr[j] > key)
+		{
 			arr[j + 1] = arr[j];
 			--j;
 		}
 		arr[j + 1] = key;
 	}
 }
+
 // RandomArray Function to fill the array with random numbers
-void RandomArray(int array[], int size) {
+void RandomArray(int array[], int size)
+{
 
 	for (int i = 0; i < size; i++)
 		array[i] = rand();
-
 }
--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,47 +1,53 @@
+
+// Libraries to be added
+
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
+#include <chrono>
 #include <math.h>
 #include <time.h>
 #include <fstream>
 
 using namespace std;
+
+// Function Prototypes
+
 void MergeSort(int array[], int start, int end);
 void Merge(int array[], int p, int q, int r);
 void RandomArray(int array[], int size);
+
 int main()
 {
 	int size;
 	cout << "Enter the Size of Array: ";
 	cin >> size;
 	int *A = new int[size];
+
+	// To generate random Numbers and store them
+
 	srand(time(NULL));
 	RandomArray(A, size);
 
-	/*cout << "\n Numbers before Sorting\n to print them, ";
-	system("pause");
-	for (int i = 0; i < size; i++)
-		cout << A[i] << endl;
-*/
-	clock_t start, end;
-	cout << "check1\n";
-	start = clock();
+	// Code to call MergeSort and measure time duration
+
+	auto start = chrono::high_resolution_clock::now();
 	MergeSort(A, 0, size - 1);
-	end = clock();
-	cout << "check2\n";
-	float time_taken = float(end - start) / float(CLOCKS_PER_SEC);
-	cout << "\n Time taken by the Merge Sort,\nwith " << size << " size of Array is: ";
-	cout << fixed << time_taken << setprecision(7) << " seconds" << endl;
+	auto end = chrono::high_resolution_clock::now();
+	double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+	// Code to calculate the time duration
+
+	time_taken *= 1e-9;
+	cout << "\n Time taken by the Merge Sort, with " << size << " size of Array is: ";
+	cout << fixed << time_taken << setprecision(9) << " seconds" << endl;
+
+	// Code to Save sorted array in csv file
 
 	ofstream OutData;
 	OutData.open("SortedMergeSort.csv");
 	for (int i = 0; i < size; i++)
 		OutData << A[i] << endl;
-
-	/*cout << "\n\n Numbers after Sorting\n To print them, ";
-	system("pause");
-	for (int i = 0; i < size; i++)
-		cout << A[i] << endl;*/
 
 	delete[] A;
 	return 0;
@@ -54,11 +60,15 @@ void MergeSort(int arr[], int startIndex, int endIndex)
 	if (startIndex < endIndex)
 	{
 		int q = floor((startIndex + endIndex) / 2);
+		// To divide the unsorted Array
 		MergeSort(arr, startIndex, q);
 		MergeSort(arr, q + 1, endIndex);
+		// To merge the sorted array
 		Merge(arr, startIndex, q, endIndex);
 	}
 }
+
+// Merge function to sort and merge arrays
 void Merge(int arr[], int p, int q, int r)
 {
 	int n1 = q - p + 1;
@@ -92,6 +102,7 @@ void Merge(int arr[], int p, int q, int r)
 	delete[] Left;
 	delete[] Right;
 }
+
 // RandomArray Function to fill the array with random numbers
 void RandomArray(int array[], int size)
 {
